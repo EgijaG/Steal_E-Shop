d6acc6a7c7df4dc9c11e7c46c7b06a8e504975a3
EgijaG <egija.graudina@gmail.com>
2022-09-28 00:46:53 +0300

    Added new pages, created Success and Fail payment pages

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ const products = [
     price: 23,
     category: "foto",
     reserved: false,
-    author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    author: "Jesse",
+    imgPath:
+      "https://images.unsplash.com/photo-1598983870677-01e066a0b901?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
   },
   {
     id: 1,
@@ -16,8 +17,9 @@ const products = [
     price: 97,
     category: "audio",
     reserved: false,
-    author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    author: "Gustavo Fring",
+    imgPath:
+      "https://images.unsplash.com/photo-1660330120426-0f83806543cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
   },
   {
     id: 2,
@@ -26,8 +28,9 @@ const products = [
     price: 2345,
     category: "video",
     reserved: false,
-    author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    author: "Eizenberg",
+    imgPath:
+      "https://images.unsplash.com/photo-1642918570585-e510c874a423?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
   },
   {
     id: 3,
@@ -37,7 +40,8 @@ const products = [
     category: "foto",
     reserved: false,
     author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    imgPath:
+      "https://images.unsplash.com/photo-1627646393465-93969d6a6549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80",
   },
   {
     id: 4,
@@ -47,7 +51,8 @@ const products = [
     category: "foto",
     reserved: false,
     author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    imgPath:
+      "https://images.unsplash.com/photo-1558016183-68d5d7b8851b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
   },
   {
     id: 5,
@@ -57,7 +62,8 @@ const products = [
     category: "foto",
     reserved: false,
     author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    imgPath:
+      "https://images.unsplash.com/photo-1556997685-309989c1aa82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80",
   },
   {
     id: 6,
@@ -67,7 +73,8 @@ const products = [
     category: "audio",
     reserved: false,
     author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    imgPath:
+      "https://images.unsplash.com/photo-1565551223391-be988013ee6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80",
   },
   {
     id: 7,
@@ -77,7 +84,8 @@ const products = [
     category: "video",
     reserved: false,
     author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    imgPath:
+      "https://images.unsplash.com/photo-1530497610245-94d3c16cda28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
   },
   {
     id: 8,
@@ -87,35 +95,36 @@ const products = [
     category: "video",
     reserved: false,
     author: "Brink",
-    imgPath: "img/Screenshot.jpg",
+    imgPath:
+      "https://images.unsplash.com/photo-1544377712-f7a323240581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
   },
 ];
 
 document.getElementById("myHeader").innerHTML = `
 <div class="header">
     <div class="header-items header-left">
-        <a href="/index.html" style="margin-bottom:5px"><img id="logo" src="./img/logo.png"></a>
+        <a href="index.html" style="margin-bottom:5px"><img id="logo" src="./img/logo.png"></a>
         <a class="header-link nav">My Purchases</a>
-        <a class="header-link nav" href="profils.html">My profile</a>
+        <a class="header-link nav" href="profile.html">My profile</a>
         <a class="header-link nav">Offers</a>
 
     </div>
     <div class="header-items header-right">
-        <a class="header-link login" href="/login.html" type="button">Log in</a>
-        <a class="header-link signup" href="./signup.html" type="button">Sign up</a>
+        <a class="header-link login" href="login.html" type="button">Log in</a>
+        <a class="header-link signup" href="signup.html" type="button">Sign up</a>
     </div>
 </div>
 `;
 document.getElementById("myFooter").innerHTML = `
 <div class="footer">
-    <p class="footer-item">Help</p>
-    <p class="footer-item">Contact us</p>
-    <p class="footer-item">About us</p>
+    <a class="footer-item" href="help.html">Help</p>
+    <a class="footer-item" href="contactUs.html">Contact us</p>
+    <a class="footer-item" href="login.html">About us</p>
 </div>
 `;
 
 function check() {
-  console.log(document.getElementsByClassName("btnSubmit"));
+  console.log("Jess papi, you clicked me!");
 }
 
 function renderCards() {
@@ -125,9 +134,10 @@ function renderCards() {
     div.innerHTML = `
     <div id="card">
     <img src="${p.imgPath}" alt="screenshot">
-    <h3>${p.name}</h3>
+    <h3 class="card-title">${p.name}</h3>
     <p>${p.description}</p>
-    <button type="button" class="buyButton" >${p.price} $</button>
+    <p style="font-style:italic;">By ${p.author}</p>
+    <button type="button" class="buyButton" onclick="check()">Buy for: ${p.price} $</button>
     </div>`;
     document.getElementsByClassName(p.category)[0].appendChild(div);
   });
