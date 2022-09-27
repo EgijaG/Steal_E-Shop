a4e6123430c14346eb3e7d6fed5cd1c71281a57d
EgijaG <egija.graudina@gmail.com>
2022-09-27 19:02:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,15 +44,15 @@ const products = [
 document.getElementById("myHeader").innerHTML = `
 <div class="header">
     <div class="header-items header-left">
-      <a href="/index.html" style="margin-bottom:5px"><img id="logo" src="./img/logo.png"></a>
+      <a href="index.html" style="margin-bottom:5px"><img id="logo" src="./img/logo.png"></a>
       <a class="header-link nav">My Purchases</a>
       <a class="header-link nav" href="profils.html">My profile</a>
       <a class="header-link nav">Offers</a>
 
     </div>
     <div class="header-items header-right">
-      <a class="header-link login" href="/login.html" type="button">Log in</a>
-      <a class="header-link signup" href="./signup.html" type="button">Sign up</a>
+      <a class="header-link login" href="login.html" type="button">Log in</a>
+      <a class="header-link signup" href="signup.html" type="button">Sign up</a>
     </div>
   </div>
 `;
