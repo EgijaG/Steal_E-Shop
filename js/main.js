c1f6f7120d34dc1e2b1a4f425f2b3c1ff98d8bd3
mja <mjausmanis@gmail.com>
2022-09-27 17:16:07 +0300

    Pushed what I made before

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("myHeader").innerHTML = `
+    <div class="header">
+        <div class="header-items header-left">
+            <a href="/index.html"><img id="logo" src="Images/rat.jpg"></a>
+            <a class="nav-link" href="/profils.html">Mani pirkumi</a>
+            <a class="nav-link" href="/profils.html">Mans profils</a>
+            <a class="nav-link" href="/profils.html">Piedāvājums</a>
+        </div>
+        <div class="header-items header-right">
+            <a class="nav-link" href="/login.html">Login</a>
+            <a class="nav-link" href="/signup.html">Sign up</a>
+        </div>
+    </div>
+`;
+document.getElementById("myFooter").innerHTML = `
+<div class="footer">
+    <p class="footer-item">Help</p>
+    <p class="footer-item">Contact us</p>
+    <p class="footer-item">About us</p>
+</div>
+`;
