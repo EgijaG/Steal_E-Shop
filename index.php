a239b6995dbf84ad8f4ca3de4c21abcff0d95693
EgijaG <57557140+EgijaG@users.noreply.github.com>
2022-09-29 00:16:57 +0300

    Cards/categories generating in accordance w/ JSON

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Steal</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
 
 </head>
 
-<body onload="renderCards()">
+<body onload="renderCategories()">
     <div id="myHeader"></div>
 
     <div class="content">
@@ -39,36 +38,14 @@
             </div>
         </div>
 
-
-
-        <div class="foto row cards">
-            <div class="section-title">
-                <h5>Foto</h5>
-            </div>
-
-        </div>
-
-        <div class="audio row cards">
-            <div class="section-title">
-                <h5>Audio</h5>
-            </div>
-        </div>
-
-        <div class="video row cards">
-            <div class="section-title">
-                <h5>Video</h5>
-            </div>
-
-        </div>
+        <div class='fuck'></div>
 
     </div>
 
     <div id="myFooter"></div>
 
     <script src="./js/main.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- js/main.js
+++ js/main.js
@@ -1,105 +1,14 @@
-const products = [
-  {
-    id: 0,
-    name: "Nude",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 23,
-    category: "foto",
-    reserved: false,
-    author: "Jesse",
-    imgPath:
-      "https://images.unsplash.com/photo-1598983870677-01e066a0b901?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-  },
-  {
-    id: 1,
-    name: "Jane",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 97,
-    category: "audio",
-    reserved: false,
-    author: "Gustavo Fring",
-    imgPath:
-      "https://images.unsplash.com/photo-1660330120426-0f83806543cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
-  },
-  {
-    id: 2,
-    name: "Jack",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 2345,
-    category: "video",
-    reserved: false,
-    author: "Eizenberg",
-    imgPath:
-      "https://images.unsplash.com/photo-1642918570585-e510c874a423?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
-  },
-  {
-    id: 3,
-    name: "Jill",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 14,
-    category: "foto",
-    reserved: false,
-    author: "Brink",
-    imgPath:
-      "https://images.unsplash.com/photo-1627646393465-93969d6a6549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80",
-  },
-  {
-    id: 4,
-    name: "Jill",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 14,
-    category: "foto",
-    reserved: false,
-    author: "Brink",
-    imgPath:
-      "https://images.unsplash.com/photo-1558016183-68d5d7b8851b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-  },
-  {
-    id: 5,
-    name: "Jill",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 14,
-    category: "foto",
-    reserved: false,
-    author: "Brink",
-    imgPath:
-      "https://images.unsplash.com/photo-1556997685-309989c1aa82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80",
-  },
-  {
-    id: 6,
-    name: "Jill",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 14,
-    category: "audio",
-    reserved: false,
-    author: "Brink",
-    imgPath:
-      "https://images.unsplash.com/photo-1565551223391-be988013ee6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80",
-  },
-  {
-    id: 7,
-    name: "Jill",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 14,
-    category: "video",
-    reserved: false,
-    author: "Brink",
-    imgPath:
-      "https://images.unsplash.com/photo-1530497610245-94d3c16cda28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
-  },
-  {
-    id: 8,
-    name: "Jill",
-    description: "Lorem ipsum dolor Lorem ipsum dolor sit amet",
-    price: 14,
-    category: "video",
-    reserved: false,
-    author: "Brink",
-    imgPath:
-      "https://images.unsplash.com/photo-1544377712-f7a323240581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
-  },
-];
-
+let categories = [];
+fetch("data/products.json")
+    .then((resp) => resp.json())
+    .then((prod) =>
+      prod.forEach((p) => {
+        if (!categories.includes(p.category)) {
+          categories.push(p.category);
+          console.log(p.category);
+        }
+      })
+    );
 document.getElementById("myHeader").innerHTML = `
 <div class="header">
     <div class="header-items header-left">
@@ -123,37 +32,51 @@ document.getElementById("myFooter").innerHTML = `
 </div>
 `;
 
-function check() {
-  console.log("Jess papi, you clicked me!");
-}
-
 theUrl2 = "http://stealstoragehost.epizy.com/getAllRows.php";
-theUrl = "https://random-data-api.com/api/v2/users"; 
+theUrl = "https://random-data-api.com/api/v2/users";
 
-function back(){
-  fetch(theUrl2,{
-    method : 'GET',
-    mode: 'no-cors',
+function back() {
+  fetch(theUrl2, {
+    method: "GET",
+    mode: "no-cors",
     headers: {
-    'Content-Type': 'application/json'
-  },
+      "Content-Type": "application/json",
+    },
   })
-  .then(res => res.json())
-  .then(json => JSON.parse());
+    .then((res) => res.json())
+    .then((json) => JSON.parse());
   console.log(json);
 }
 
-async function test(){
+async function test() {
   const resp = await fetch(theUrl);
-  const object = await resp.json();
+  let object = await resp.json();
   console.log(object);
 }
 
+ function renderCategories() { 
+  categories.forEach((c) => {
+    let cat = document.createElement("div");
+    cat.id = "category";
+    cat.innerHTML = `
+          <div class="${c} row cards">
+              <div class="section-title">
+                  <h5>${c}</h5>
+              </div>
+          </div>`;
+    document.getElementsByClassName("fuck")[0].appendChild(cat);
+  });
+  renderCards();
+}
+
 function renderCards() {
-  products.forEach((p) => {
-    let div = document.createElement("div");
-    div.id = "renderedCard";
-    div.innerHTML = `
+  fetch("data/products.json")
+    .then((resp) => resp.json())
+    .then((prod) =>
+      prod.forEach((p) => {
+        let div = document.createElement("div");
+        div.id = "renderedCard";
+        div.innerHTML = `
     <div id="card">
     <img src="${p.imgPath}" alt="screenshot">
     <h3 class="card-title">${p.name}</h3>
@@ -161,6 +84,8 @@ function renderCards() {
     <p style="font-style:italic;">By ${p.author}</p>
     <button type="button" onclick="test()">Buy for: ${p.price} $</button>
     </div>`;
-    document.getElementsByClassName(p.category)[0].appendChild(div);
-  });
+        document.getElementsByClassName(p.category)[0].appendChild(div);
+      })
+    );
+  return;
 }
