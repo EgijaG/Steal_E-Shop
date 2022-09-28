13ed9ff5e04c53dbd1964873e12f30e61079d3d9
BeaT <helvis.tics00@gmail.com>
2022-09-28 21:26:30 +0300

    database connection from signup page

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "mouse.db.elephantsql.com";
+$dbUsername = "yogblwsn";
+$dbPassword = "Y7W55OMonYfWrDAhBQp70cDTGHZCysiU";
+
+try {
+    $conn = new PDO("pgsql:host=$servername;dbname=$dbUsername", $dbUsername, $dbPassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+?>
